73598cc720accf596fe89ac4cc44ddcdbb7734d4
rhailrake <49613070+rhailrake@users.noreply.github.com>
2022-12-06 05:23:35 +0600

    finished mb? or i forget something..

--- Tools/ss14_ru/keyfinder.py
+++ Tools/ss14_ru/keyfinder.py
@@ -58,8 +58,8 @@ class FilesFinder:
                 self.created_files.append(ru_file)
             elif relative_file.locale == 'ru-RU':
                 is_engine_files = "robust-toolbox" in (relative_file.file.full_path)
-                is_corvax_files = "corvax" in (relative_file.file.full_path)
-                if not is_engine_files and not is_corvax_files:
+                is_white_files = "white" in (relative_file.file.full_path)
+                if not is_engine_files and not is_white_files:
                     self.warn_en_analog_not_exist(relative_file)
             else:
                 raise Exception(f'Файл {relative_file.file.full_path} имеет неизвестную локаль "{relative_file.locale}"')
