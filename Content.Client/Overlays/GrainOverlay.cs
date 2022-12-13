dd15173ea09157ffcda8494a41b3c1d1429953f4
rhailrake <49613070+rhailrake@users.noreply.github.com>
2022-12-13 13:12:28 +0600

    fix

--- Content.Client/Overlays/GrainOverlay.cs
+++ Content.Client/Overlays/GrainOverlay.cs
@@ -1,12 +1,16 @@
 ﻿using Robust.Client.Graphics;
 using Robust.Shared.Enums;
 using Robust.Shared.Prototypes;
+using Robust.Client.UserInterface;
+
 
 namespace Content.Client.Overlays;
 
 public sealed class GrainOverlay : Overlay
 {
     [Dependency] private readonly IPrototypeManager _prototype = default!;
+    [Dependency] private readonly IUserInterfaceManager _interface = default!;
+
     public override OverlaySpace Space => OverlaySpace.WorldSpace;
     public override bool RequestScreenTexture => true;
 
@@ -26,7 +30,7 @@ public sealed class GrainOverlay : Overlay
         var screenHandle = args.WorldHandle;
 
         _shader.SetParameter("SCREEN_TEXTURE", ScreenTexture);
-        _shader.SetParameter("strength", 50.0f);
+        _shader.SetParameter("strength", _interface.RootControl.UIScale * 15.0f);
 
         screenHandle.UseShader(_shader);
         screenHandle.DrawRect(args.WorldBounds, Color.White);
