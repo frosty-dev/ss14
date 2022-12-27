46ad736354303e8cdb04b00191f30d2c5e8cf89b
RavMorgan <48182970+RavMorgan@users.noreply.github.com>
2022-12-27 22:53:31 +0300

    Sockets (#66)
    Утка сокеты
    
    Co-authored-by: Олежа Блаженный <null>

---
+++ Content.Server/UtkaIntegration/Commands/IUtkaCommand.cs
@@ -0,0 +1,7 @@
+﻿namespace Content.Server.UtkaIntegration;
+
+public interface IUtkaCommand
+{
+    string Name { get; }
+    public void Execute(UtkaSocket socket, FromDiscordMessage message, string[] args);
+}
---
+++ Content.Server/UtkaIntegration/Commands/UtkaSendOOCMessage.cs
@@ -0,0 +1,14 @@
+﻿using Content.Server.Chat.Managers;
+
+namespace Content.Server.UtkaIntegration;
+
+public sealed class UtkaSendOOCMessage : IUtkaCommand
+{
+    public string Name => "OOC";
+    public void Execute(UtkaSocket socket, FromDiscordMessage message, string[] args)
+    {
+        var chatSystem = IoCManager.Resolve<IChatManager>();
+
+        chatSystem.SendHookOOC($"{message.Ckey}", $"{message.Message}");
+    }
+}
