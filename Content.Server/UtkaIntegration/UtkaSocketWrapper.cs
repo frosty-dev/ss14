a09f6c17bf4b5b9de73bb34fdfac97c658bff680
RavMorgan <48182970+RavMorgan@users.noreply.github.com>
2022-12-29 16:52:07 +0300

    Sockets (#73)
    Сокеты

---
+++ Content.Server/UtkaIntegration/UtkaSocketWrapper.cs
@@ -0,0 +1,49 @@
+﻿using Content.Shared.CCVar;
+using Robust.Shared;
+using Robust.Shared.Configuration;
+
+namespace Content.Server.UtkaIntegration;
+
+public sealed class UtkaSocketWrapper
+{
+    [Dependency] private readonly IConfigurationManager _cfg = default!;
+
+    private UtkaSocket? _utkaSocket;
+
+    private bool _initialized;
+    private string _key = string.Empty;
+
+    public void Initialize()
+    {
+        if(_initialized) return;
+
+        _cfg.OnValueChanged(CCVars.UtkaSocketKey, value => _key = value, true);
+
+        if(string.IsNullOrEmpty(_key)) return;
+
+        var port = _cfg.GetCVar(CVars.NetPort) + 100;
+
+        try
+        {
+            _utkaSocket = new UtkaSocket("0.0.0.0", port, _key);
+
+        }
+        catch (Exception e)
+        {
+            Logger.GetSawmill("utkasockets").Error($"Failed to initialize UtkaSocket: {e}");
+            return;
+        }
+
+        _utkaSocket.Start();
+
+        _initialized = true;
+    }
+
+    public void Shutdown()
+    {
+        _utkaSocket?.Stop();
+        _utkaSocket!.Dispose();
+
+        _initialized = false;
+    }
+}
