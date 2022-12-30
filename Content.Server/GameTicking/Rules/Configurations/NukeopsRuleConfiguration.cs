9ef4820c2f559e3e07c251d2b7869a8a66ae27d1
Kotovskiy34 <77529717+wCATw@users.noreply.github.com>
2022-12-30 15:08:13 +0300

    Нюка на 15

--- Content.Server/GameTicking/Rules/Configurations/NukeopsRuleConfiguration.cs
+++ Content.Server/GameTicking/Rules/Configurations/NukeopsRuleConfiguration.cs
@@ -15,7 +15,7 @@ public sealed class NukeopsRuleConfiguration : GameRuleConfiguration
     public override string Id => "Nukeops";
 
     [DataField("minPlayers")]
-    public int MinPlayers = 5;
+    public int MinPlayers = 15;
 
     /// <summary>
     ///     This INCLUDES the operatives. So a value of 3 is satisfied by 2 players & 1 operative
