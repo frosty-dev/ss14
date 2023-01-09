0d7e68812ee52eb318d8b8f7c3bc3f5cdc36a201
rhailrake <49613070+rhailrake@users.noreply.github.com>
2023-01-10 02:21:04 +0600

    extract tts code to partial class

---
+++ Content.Client/White/TTS/HumanoidProfileEditor.TTS.cs
@@ -0,0 +1,67 @@
+﻿using System.Linq;
+using Content.Client.White.Sponsors;
+using Content.Shared.Humanoid;
+using Content.Shared.White.TTS;
+
+namespace Content.Client.Preferences.UI;
+
+public sealed partial class HumanoidProfileEditor
+{
+    private List<TTSVoicePrototype> _voiceList = default!; // Corvax-TTS
+
+    private void InitializeVoice()
+    {
+        _voiceList = _prototypeManager.EnumeratePrototypes<TTSVoicePrototype>().Where(o => o.RoundStart).ToList();
+
+        _voiceButton.OnItemSelected += args =>
+        {
+            _voiceButton.SelectId(args.Id);
+            SetVoice(_voiceList[args.Id].ID);
+        };
+
+        _voicePlayButton.OnPressed += _ => { PlayTTS(); };
+    }
+
+    private void UpdateTTSVoicesControls()
+    {
+        if (Profile is null)
+            return;
+
+        var sponsorsManager = IoCManager.Resolve<SponsorsManager>();
+
+        _voiceButton.Clear();
+
+        var firstVoiceChoiceId = 1;
+        for (var i = 0; i < _voiceList.Count; i++)
+        {
+            var voice = _voiceList[i];
+            if (voice.Sex != Sex.Unsexed && voice.Sex != Profile.Sex)
+                continue;
+
+            var name = Loc.GetString(voice.Name);
+            _voiceButton.AddItem(name, i);
+
+            if (firstVoiceChoiceId == 1)
+                firstVoiceChoiceId = i;
+
+            if (voice.SponsorOnly &&
+                sponsorsManager.TryGetInfo(out var sponsor) &&
+                !sponsor.AllowedMarkings.Contains(voice.ID))
+            {
+                _voiceButton.SetItemDisabled(i, true);
+            }
+        }
+
+        var voiceChoiceId = _voiceList.FindIndex(x => x.ID == Profile.Voice);
+        if (!_voiceButton.TrySelectId(voiceChoiceId) &&
+            _voiceButton.TrySelectId(firstVoiceChoiceId))
+        {
+            SetVoice(_voiceList[firstVoiceChoiceId].ID);
+        }
+    }
+
+    private void PlayTTS()
+    {
+
+    }
+}
